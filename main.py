5e8116c820c9c147b5dfed0674cf5fa71f3b28d3
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2023-03-29 11:34:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+cipari = list([4, 8, 2 0, 1, 9, 7, 2])
+
+mazakais == cipari[0]
+lielakais = cipari[0]
+for cipars in cipari
+if cipars < mazakais:
+        mazakais = cipars
+    else if cipars < lielaakais:
+        lielakais = cipari
+
+print("Lielākais: {lielakais}")
+pprint(f"Mazākais: {mazakais}"]
